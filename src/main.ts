0bd0af13bba98461303f300891fb5e737ff68b9c
Oliver Borner <oliverborner@gmail.com>
2023-06-10 11:47:43 +0200

    changed back

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+
 const app = createApp(App)
 
 app.use(createPinia())
 
 app.mount('#app')
 
-(function prependBase() {
-  document.querySelectorAll("a").forEach((link) => {
-    let url = link.getAttribute("href");
-    if (url?.startsWith("/")) {
-      url = import.meta.env.BASE_URL + url.slice(1);
-      link.setAttribute("href", url);
-    }
-  });
-})();
